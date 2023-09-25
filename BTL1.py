d201d3ea52fc6cd950e8888c605c7e7337c5ee63
minhkakart <nguyenminh2732@gmail.com>
2023-09-25 15:27:35 +0700

    bieu do

--- BTL1.py
+++ BTL1.py
@@ -4,6 +4,7 @@ from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from sklearn.model_selection import train_test_split, KFold
 from hydroeval import evaluator, nse
+from matplotlib import pyplot as plt
 
 ### Hàm đánh giá Nash-Sutcliffe efficiency (NSE)
 def nse(targets, predictions):
@@ -42,14 +43,15 @@ def linear():
     linearReg.fit(trainFoldListX[fitDataIndex],trainFoldListY[fitDataIndex]) ## Retrain model with the data
     predict = linearReg.predict(xTest) ## Predict with test data
     train_set = linearReg.predict(xTrain) ## Predict with train data
-    return {'Title':'LinearRegression k-fold coss validation',
-    'R2 score': r2_score(yTest, predict),
-    'Score NSE': nse(yTest, predict),
-    'Score NSE by hydroeval': evaluator(nse, predict, yTest),
-    'Score MAE': mean_absolute_error(yTest, predict),
-    'Score RMSE': mean_squared_error(yTest, predict)**0.5,
-    'Test error': train_test_err(yTest.tolist(), predict.tolist()),
-    'Train error': train_test_err(yTrain.tolist(), train_set.tolist())}
+    # return {'Title':'LinearRegression k-fold coss validation',
+    # 'R2 score': r2_score(yTest, predict),
+    # 'Score NSE': nse(yTest, predict),
+    # 'Score NSE by hydroeval': evaluator(nse, predict, yTest),
+    # 'Score MAE': mean_absolute_error(yTest, predict),
+    # 'Score RMSE': mean_squared_error(yTest, predict)**0.5,
+    # 'Test error': train_test_err(yTest.tolist(), predict.tolist()),
+    # 'Train error': train_test_err(yTrain.tolist(), train_set.tolist())}
+    return predict.tolist()
             
 ### Lasso
 def lasso():
@@ -57,14 +59,16 @@ def lasso():
     lassoReg.fit(xTrain, yTrain)
     predict = numpy.array(lassoReg.predict(xTest))
     train_set = lassoReg.predict(xTrain)
-    return {'Title': 'Lasso',
-    'R2 score': r2_score(yTest, predict),
-    'Score NSE': nse(yTest, predict),
-    'Score NSE by hydroeval': evaluator(nse, predict, yTest),
-    'Score MAE': mean_absolute_error(yTest, predict),
-    'Score RMSE': mean_squared_error(yTest, predict)**0.5,
-    'Test error': train_test_err(yTest.tolist(), predict.tolist()),
-    'Train error': train_test_err(yTrain.tolist(), train_set.tolist())}
+    # return {'Title': 'Lasso',
+    # 'R2 score': r2_score(yTest, predict),
+    # 'Score NSE': nse(yTest, predict),
+    # 'Score NSE by hydroeval': evaluator(nse, predict, yTest),
+    # 'Score MAE': mean_absolute_error(yTest, predict),
+    # 'Score RMSE': mean_squared_error(yTest, predict)**0.5,
+    # 'Test error': train_test_err(yTest.tolist(), predict.tolist()),
+    # 'Train error': train_test_err(yTrain.tolist(), train_set.tolist())}
+    return predict.tolist()
+
 
 ### Ridge
 def ridge():
@@ -72,14 +76,16 @@ def ridge():
     ridgeReg.fit(xTrain, yTrain)
     predict = ridgeReg.predict(xTest)
     train_set = ridgeReg.predict(xTrain)
-    return {'Title': 'Ridge',
-    'R2 score': r2_score(yTest, predict),
-    'Score NSE': nse(yTest, predict),
-    'Score NSE by hydroeval': evaluator(nse, predict, yTest),
-    'Score MAE': mean_absolute_error(yTest, predict),
-    'Score RMSE': mean_squared_error(yTest, predict)**0.5,
-    'Test error': train_test_err(yTest.tolist(), predict.tolist()),
-    'Train error': train_test_err(yTrain.tolist(), train_set.tolist())}
+    # return {'Title': 'Ridge',
+    # 'R2 score': r2_score(yTest, predict),
+    # 'Score NSE': nse(yTest, predict),
+    # 'Score NSE by hydroeval': evaluator(nse, predict, yTest),
+    # 'Score MAE': mean_absolute_error(yTest, predict),
+    # 'Score RMSE': mean_squared_error(yTest, predict)**0.5,
+    # 'Test error': train_test_err(yTest.tolist(), predict.tolist()),
+    # 'Train error': train_test_err(yTrain.tolist(), train_set.tolist())}
+    return predict.tolist()
+
 
 data = pandas.read_csv('Student_Performance.csv') ## Load data
 
@@ -95,3 +101,47 @@ xTest, yTest = numpy.array(dTest.iloc[:,:5]), numpy.array(dTest.iloc[:,5])  ## D
 #     for j in i:
 #         print(j, ': ', i[j], sep='')
 #     print()
+ 
+li = linear()
+la = lasso()
+ri = ridge()
+
+plt.suptitle('BLT')
+## Study hours
+plt.subplot(2, 3, 1)
+plt.title('LinearReg')
+plt.ylabel('Performance index')
+plt.plot(xTest[:100, 0], yTest[:100], 'ro')
+plt.plot(xTest[:100, 0], li[:100], 'y>')
+
+plt.subplot(2, 3, 2)
+plt.title('Ridge')
+plt.plot(xTest[:100, 0], yTest[:100], 'mo')
+plt.plot(xTest[:100, 0], ri[:100], 'g<')
+
+
+plt.subplot(2, 3, 3)
+plt.title('Lasso')
+plt.plot(xTest[:100, 0], yTest[:100], 'mo')
+plt.plot(xTest[:100, 0], la[:100], 'b^')
+
+##Previous Scores
+plt.subplot(2, 3, 4)
+# plt.title('LinearReg')
+plt.ylabel('Performance index')
+plt.plot(xTest[:100, 1], yTest[:100], 'ro')
+plt.plot(xTest[:100, 1], li[:100], 'y>')
+
+plt.subplot(2, 3, 5)
+# plt.title('Ridge')
+plt.plot(xTest[:100, 1], yTest[:100], 'mo')
+plt.plot(xTest[:100, 1], ri[:100], 'g<')
+
+
+plt.subplot(2, 3, 6)
+# plt.title('Lasso')
+plt.plot(xTest[:100, 1], yTest[:100], 'mo')
+plt.plot(xTest[:100, 1], la[:100], 'b^')
+
+
+plt.show()
